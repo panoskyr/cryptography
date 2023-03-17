3655f064fd981ad0bbe1de8f3c777d4bb2734736
panoskyr <35936229+panoskyr@users.noreply.github.com>
2023-03-17 17:20:50 +0200

    made cleantext and preprocess works

--- vigenere.py
+++
@@ -1,9 +0,0 @@
-#function to make a normal text into all capital 
-#no punctuation . It needs to be between 350 and 500 words
-import re
-def makeupper(s):
-    s="sting. that'' has puncd!"
-    s=re.sub(r'[^\w]','', s)
-    s=s.upper()
-    print(s)
-    return s
---
+++ vigenere_preprocess.py
@@ -0,0 +1,23 @@
+#function to make a normal text into all capital 
+#no punctuation . It needs to be between 350 and 500 words
+import re
+def makeupper(s):
+    #s="sting. that'' has puncd!"
+    s=re.sub(r'[^a-zA-z]','', s)
+    s=s.upper()
+    #print(s)
+    return s
+
+
+
+with open('plaintext.txt', 'r') as file:
+    data=file.read().replace('\n','')
+    print (type(data))
+    #print (data)
+    data_upper=makeupper(data)
+    print (data_upper)
+
+
+#write the clean all uppercase file
+with open('clean_plaintext.txt', 'w') as file:
+    file.write(data_upper)
